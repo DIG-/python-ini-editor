87172ffa07f47aa289e189e70a469d6112e27f58
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-10-22 09:24:15 -0300

    Implemented editor

--- src/dig_ini_editor/editor.py
+++ src/dig_ini_editor/editor.py
@@ -1,5 +1,5 @@
 from configparser import ConfigParser
-from typing import TextIO
+from typing import TextIO, Optional
 
 __all__ = ["Editor"]
 
@@ -16,3 +16,28 @@ class Editor:
 
     def write(self, stream: TextIO) -> None:
         self._config.write(stream)
+
+    def get(self, section: Optional[str], key: Optional[str], separator: str) -> str:
+        if section is None:
+            return separator.join(self._config.sections())
+        if key is None:
+            return separator.join(self._config.options(section))
+        return self._config.get(section, key)
+
+    def set(self, section: str, key: str, value: str) -> None:
+        if not self._config.has_section(section):
+            self._config.add_section(section)
+        self._config.set(section, key, value)
+
+    def delete(self, section: str, key: Optional[str]) -> None:
+        if key is None:
+            if self._config.has_section(section):
+                self._config.remove_section(section)
+            return
+        if self._config.has_option(section, key):
+            self._config.remove_option(section, key)
+
+    def contains(self, section: str, key: Optional[str]) -> bool:
+        if key is None:
+            return self._config.has_section(section)
+        return self._config.has_option(section, key)
