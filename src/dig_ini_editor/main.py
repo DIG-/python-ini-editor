14135e1c324a44597b999009373b9da6fa1f9de7
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-10-22 06:51:36 -0300

    Added version variable

--- src/dig_ini_editor/main.py
+++ src/dig_ini_editor/main.py
@@ -1,8 +1,15 @@
 import sys
 from typing import List
-from .arguments import create_argument_parser, Arguments
+from . import __version__
+from .arguments import create_argument_parser, Arguments, Argument
 
 
 def main(args: List[str] = sys.argv[1:]) -> None:
-    arguments = create_argument_parser().parse_args(args, Arguments())
+    parser = create_argument_parser()
+    arguments = parser.parse_args(args, Arguments())
+
+    if arguments.get_boolean(Argument.VERSION):
+        print(f"{parser.prog} {__version__}")
+        return
+
     print(arguments)
